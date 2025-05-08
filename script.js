0080b007eccd58b17de06168efec1cb793fc96f1
Gevork Eminyan <gevork@eminyan.ru>
2025-05-08 17:41:52 +0400

    70% ready. Full page without adaptive resolution. Form ib footer and in header works

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //Java Script Here 
+
+// Год в футере
+document.addEventListener("DOMContentLoaded", ()=> {
+    const year = new Date().getFullYear();
+    const yearElement = document.getElementById("current-year");
+    if (yearElement) {
+        yearElement.textContent = year;
+    }
+});
+
+// Модальное окно
+document.addEventListener("DOMContentLoaded", () => {
+    const modal = document.getElementById("modal-form");
+    const overlay = document.getElementById("overlay");
+    const openModalButton = document.querySelector(".header__form");
+    const closeModalButton = document.querySelector(".close-modal");
+
+    openModalButton.addEventListener("click", () => {
+        modal.classList.add("show");
+        overlay.classList.add("show");
+    });
+
+    closeModalButton.addEventListener("click", () => {
+        modal.classList.remove("show");
+        overlay.classList.remove("show");
+    });
+
+    overlay.addEventListener("click", () => {
+        modal.classList.remove("show");
+        overlay.classList.remove("show");
+    });
+});
